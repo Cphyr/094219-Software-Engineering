902ed044f1baf85be3a7f1c0d29a8d7b5bf049dd
Cphyr <cfir.hadar@gmail.com>
2021-06-22 23:36:41 +0300

    docstring of folder.java

--- HW2/src/Folder.java
+++ HW2/src/Folder.java
@@ -7,25 +7,41 @@ public class Folder extends StorageItem {
 
     private ArrayList<StorageItem> items;
 
-    public Folder(String name){
+    public Folder(String name) {
+        /**
+         * @param name (String) the name of the folder
+         * @return Constructor
+         */
         super(name);
         this.items = new ArrayList<StorageItem>();
     }
 
-
     public int getSize() {
+        /**
+         * @return (int) sum over all item's length in the folder recursivly where
+         *         length of a file is the len of the content of a file
+         */
         int len = 0;
-        for (StorageItem item : items){
-            len += item.getSize();
+        for (StorageItem item : items) {
+            len += item.getSize(); // recursive call
         }
         return len;
     }
 
     public ArrayList<StorageItem> getItems() {
+        /**
+         * @return the items in the folder
+         */
         return items;
     }
 
     public boolean addItem(StorageItem item) {
+        /**
+         * @param item (StorageItem) an item to add to the folder
+         * @return (boolean) true if succeeded, false otherwise, where success is
+         *         defined as if the there was an item of this name in the folder
+         *         already
+         */
         String itemName = item.getName();
         for (StorageItem i : items) {
             if (i.getName().equals(itemName))
@@ -35,36 +51,35 @@ public class Folder extends StorageItem {
         return true;
     }
 
-
-
-    public File findFile(String path){
-        String next="";
-        int curPlace=0, length = path.length();
-        Folder currFolder=this;
-        boolean didChange=false;
-        while(true){
-            while(curPlace<length && path.charAt(curPlace) != '/'  ){
-                next+=path.charAt(curPlace);
+    public File findFile(String path) {
+        /**
+         * @param path (String) the path to find in the folder
+         * @return (File) The file with that path. If none were found return null
+         */
+        String next = "";
+        int curPlace = 0, length = path.length();
+        Folder currFolder = this;
+        boolean didChange = false;
+        while (true) {
+            while (curPlace < length && path.charAt(curPlace) != '/') {
+                next += path.charAt(curPlace);
                 curPlace++;
             }
             curPlace++;
-            didChange=false;
-            for(StorageItem s: currFolder.items){
-                if(s.getName().equals(next)){
-                    if(s instanceof File)
-                        return (File)s;
-                    currFolder=(Folder)s;
-                    didChange=true;
+            didChange = false;
+            for (StorageItem s : currFolder.items) {
+                if (s.getName().equals(next)) {
+                    if (s instanceof File)
+                        return (File) s;
+                    currFolder = (Folder) s;
+                    didChange = true;
                     break;
                 }
             }
-            if(! didChange)
+            if (!didChange)
                 return null;
-            next="";
+            next = "";
         }
     }
 
-
-
 }
-
--- HW2/src/StorageItem.java
+++ HW2/src/StorageItem.java
@@ -12,16 +12,17 @@ abstract public class StorageItem {
     public StorageItem(String name) {
         /**
          * Makes a StorageItem
-         * @param   name    (string) the StorageItem's name
+         * 
+         * @param name (string) the StorageItem's name
          */
         this.name = name;
-        this.date = MakeDate(); // change later
+        this.date = MakeDate();
     }
 
     public long abs(long num) {
         /**
-         * Returns the absolute value
-         * @params  num     (long) the number
+         * @params num (long) the number
+         * @return (long) the abs value
          */
         if (num >= 0)
             return num;
@@ -30,11 +31,11 @@ abstract public class StorageItem {
 
     public Timestamp MakeDate() {
         /**
-         * Generates a new TimeStamp
+         * @returns (Timestamp) random timestamp
          */
         Timestamp first = Timestamp.valueOf("2017-1-1 00:00:00");
         Timestamp second = Timestamp.valueOf("2021-12-31 23:59:59");
-        //  calculates the number of milliseconds between the dates/
+        // calculates the number of milliseconds between the dates/
         long numOfMilli = second.getTime() - first.getTime();
         long mill = Main.rnd.nextLong();
         mill = abs(mill % numOfMilli);
@@ -53,10 +54,11 @@ abstract public class StorageItem {
     public void sortTree(SortingField field) {
         /**
          * Sorts the tree based on a given SortingField
-         * @params  field   (SortingField) the SortingField to sort by.
+         * 
+         * @params field (SortingField) the SortingField to sort by.
          */
         Comparator<StorageItem> comparator;
-        //  Makes the correct Comparator object;
+        // Makes the correct Comparator object;
         if (field == SortingField.DATE) {
             comparator = Comparator.comparing(StorageItem::getDate).thenComparing(StorageItem::getName);
         } else if (field == SortingField.NAME) {
@@ -72,16 +74,18 @@ abstract public class StorageItem {
     public void printTree(SortingField field) {
         /**
          * Calls a different PrintTree
-         * @param   field   (SortingField) the SortingField to sort by.
+         * 
+         * @param field (SortingField) the SortingField to sort by.
          */
         printTree(0, field);
     }
 
     public void printTree(int depth, SortingField field) {
         /**
-         *  Prints the tree
-         * @param   depth   (int) the depth in the tree the StorageItem is
-         * @param   field   (SortingField) the SortingField to sort by.
+         * Prints the tree
+         * 
+         * @param depth (int) the depth in the tree the StorageItem is
+         * @param field (SortingField) the SortingField to sort by.
          */
         sortTree(field);
         String start = "";
