70b2c4ffef32bfab02724a3d02e10c5122775189
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2021-06-22 11:32:58 +0300

    docs StorageItem

--- HW2/src/StorageItem.java
+++ HW2/src/StorageItem.java
@@ -10,22 +10,35 @@ abstract public class StorageItem {
     abstract public int getSize();
 
     public StorageItem(String name) {
+        /**
+         * Makes a StorageItem
+         * @param   name    (string) the StorageItem's name
+         */
         this.name = name;
         this.date = MakeDate(); // change later
     }
 
     public long abs(long num) {
+        /**
+         * Returns the absolute value
+         * @params  num     (long) the number
+         */
         if (num >= 0)
             return num;
         return -num;
     }
 
     public Timestamp MakeDate() {
+        /**
+         * Generates a new TimeStamp
+         */
         Timestamp first = Timestamp.valueOf("2017-1-1 00:00:00");
         Timestamp second = Timestamp.valueOf("2021-12-31 23:59:59");
+        //  calculates the number of milliseconds between the dates/
         long numOfMilli = second.getTime() - first.getTime();
         long mill = Main.rnd.nextLong();
         mill = abs(mill % numOfMilli);
+        // Generates new long number
         return new Timestamp(mill + first.getTime());
     }
 
@@ -38,7 +51,12 @@ abstract public class StorageItem {
     }
 
     public void sortTree(SortingField field) {
-        Comparator<StorageItem> comparator = Comparator.comparing(StorageItem::getSize);
+        /**
+         * Sorts the tree based on a given SortingField
+         * @params  field   (SortingField) the SortingField to sort by.
+         */
+        Comparator<StorageItem> comparator;
+        //  Makes the correct Comparator object;
         if (field == SortingField.DATE) {
             comparator = Comparator.comparing(StorageItem::getDate).thenComparing(StorageItem::getName);
         } else if (field == SortingField.NAME) {
@@ -46,15 +64,25 @@ abstract public class StorageItem {
         } else {
             comparator = Comparator.comparing(StorageItem::getSize).thenComparing(StorageItem::getName);
         }
+        // Sorts the tree
         if (this instanceof Folder)
             Collections.sort(((Folder) this).getItems(), comparator);
     }
 
     public void printTree(SortingField field) {
+        /**
+         * Calls a different PrintTree
+         * @param   field   (SortingField) the SortingField to sort by.
+         */
         printTree(0, field);
     }
 
     public void printTree(int depth, SortingField field) {
+        /**
+         *  Prints the tree
+         * @param   depth   (int) the depth in the tree the StorageItem is
+         * @param   field   (SortingField) the SortingField to sort by.
+         */
         sortTree(field);
         String start = "";
         for (int i = 0; i < depth; i++) {
